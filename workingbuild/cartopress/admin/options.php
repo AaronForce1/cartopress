4f98bf00808ab7ea196c16efb8de3b592d78e0c7
T. Andrew Hallisey <troyhallisey@gmail.com>
2015-09-15 21:40:45 -0400

    removed unnecessary code

--- workingbuild/cartopress/admin/options.php
+++ workingbuild/cartopress/admin/options.php
@@ -52,67 +52,69 @@
 		</form>
 		<p>Checking these boxes will add these features to sync so they can be featured and styled in the CartoDB infobox.</p>
 		<hr/>
+		<!--***FOR VERSION 2***
 		<h3>Publishing</h3>
-		<h4>Preview Map</h4>
-		<p>This is the map displayed when creating or updating the post. This will not be published, but is for the purpose of geolocating your posts in the Dashboard.</p>
-			<h5>Providers</h5>
-			<select id="cpdb-previewmap-select">
-				<option>Acetate</option>
-				<option>BasemapAT</option>
-				<option>CartoDB</option>
-				<option>ESRI</option>
-				<option>FreeMapSK</option>
-				<option>HERE (formerly Nokia)</option>
-				<option>HikeBike</option>
-				<option>Hydda</option>
-				<option>MapBox</option>
-				<option>MapQuest</option>
-				<option>MtbMap</option>
-				<option>NASAGIBS</option>
-				<option>OpenMapSurfer</option>
-				<option>OpenSeaMap</option>
-				<option>OpenStreetMap</option>
-				<option>OpenWeatherMap</option>
-				<option>Stamen</option>
-				<option>Thunderforest</option>
-				<option>Other</option>
-			</select>
-		<p>This is where the Map Select will go</p>
-		<h4>Single Map</h4>
-		<p>A single map is used for displaying the location of a single post, page, comment or media object. This must be inserted into the post that is being georeferenced. Use the following shortcode to insert into your post, page or comment template or insert into your post content. <strong>[cartodb_single_map]</strong>. See <a href="#">documentation</a> for shortcode options including setting the size.</p>
-		<form id="cpdb-publishtype-singlepost">
-			<table>
-				<tr>
-					<td><input type="radio" name="CartoDBJPG" />
-					</td>
-					<td><label for="CartoDBJPG">CartoDB JPG Option</label> <p>This will insert a JPG instead of a dynamic map.</p>
-					</td>
-				</tr>
-				<tr>
-					<td><input type="radio" name="iframe" />
-					</td>
-					<td><label for="iframe">Use iframe embed</label> <p>This will use CartoDB iframe embed code instead of using the API. It requires the CartoDB share URL.</p>
-					</td>
-				</tr>
-				<tr>
-					<td><label for="iframe_url">URL:</label>
-					</td>
-					<td><input type="text" name="iframe_url" placeholder="Enter CartoDB share URL"/>
-					</td>
-				</tr>
-				<tr>
-					<td><input type="radio" name="javascript" />
-					</td>
-					<td><label for="javascript">Use Javascript embed</label> <p>This allows for greater customization. This must be selected to include a layer filter and search.</p>
-					</td>
-				</tr>
-				<tr>
-					<td>CartoDB URL:
-					</td>
-					<td><input type="text" name="iframe_url" placeholder="Enter CartoDB share URL"/>
-					</td>
-				</tr>
-			</table>
-		</form>
+				<h4>Preview Map</h4>
+				<p>This is the map displayed when creating or updating the post. This will not be published, but is for the purpose of geolocating your posts in the Dashboard.</p>
+					<h5>Providers</h5>
+					<select id="cpdb-previewmap-select">
+						<option>Acetate</option>
+						<option>BasemapAT</option>
+						<option>CartoDB</option>
+						<option>ESRI</option>
+						<option>FreeMapSK</option>
+						<option>HERE (formerly Nokia)</option>
+						<option>HikeBike</option>
+						<option>Hydda</option>
+						<option>MapBox</option>
+						<option>MapQuest</option>
+						<option>MtbMap</option>
+						<option>NASAGIBS</option>
+						<option>OpenMapSurfer</option>
+						<option>OpenSeaMap</option>
+						<option>OpenStreetMap</option>
+						<option>OpenWeatherMap</option>
+						<option>Stamen</option>
+						<option>Thunderforest</option>
+						<option>Other</option>
+					</select>
+				<p>This is where the Map Select will go</p>
+				<h4>Single Map</h4>
+				<p>A single map is used for displaying the location of a single post, page, comment or media object. This must be inserted into the post that is being georeferenced. Use the following shortcode to insert into your post, page or comment template or insert into your post content. <strong>[cartodb_single_map]</strong>. See <a href="#">documentation</a> for shortcode options including setting the size.</p>
+				<form id="cpdb-publishtype-singlepost">
+					<table>
+						<tr>
+							<td><input type="radio" name="CartoDBJPG" />
+							</td>
+							<td><label for="CartoDBJPG">CartoDB JPG Option</label> <p>This will insert a JPG instead of a dynamic map.</p>
+							</td>
+						</tr>
+						<tr>
+							<td><input type="radio" name="iframe" />
+							</td>
+							<td><label for="iframe">Use iframe embed</label> <p>This will use CartoDB iframe embed code instead of using the API. It requires the CartoDB share URL.</p>
+							</td>
+						</tr>
+						<tr>
+							<td><label for="iframe_url">URL:</label>
+							</td>
+							<td><input type="text" name="iframe_url" placeholder="Enter CartoDB share URL"/>
+							</td>
+						</tr>
+						<tr>
+							<td><input type="radio" name="javascript" />
+							</td>
+							<td><label for="javascript">Use Javascript embed</label> <p>This allows for greater customization. This must be selected to include a layer filter and search.</p>
+							</td>
+						</tr>
+						<tr>
+							<td>CartoDB URL:
+							</td>
+							<td><input type="text" name="iframe_url" placeholder="Enter CartoDB share URL"/>
+							</td>
+						</tr>
+					</table>
+				</form>-->
+		
     </div>
 </div>
