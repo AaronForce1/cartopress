e3718707b5ef95ce8d1e1371392fc21d998c3cbc
T. Andrew Hallisey <troyhallisey@gmail.com>
2015-11-01 20:50:55 -0500

    Update geocoder-helper.js

--- workingbuild/cartopress/admin/js/geocoder-helper.js
+++ workingbuild/cartopress/admin/js/geocoder-helper.js
@@ -1,10 +1,12 @@
 jQuery(document).ready(function($){
 	$('input#unlock_manual_edit').click(function() {
 	    if ($(this).is(':checked')) {
-			$('#cartopress_locator #cpdb-geocode-values span input').attr('disabled', false);
+			$('#cartopress_locator #cpdb-geocode-values span input').removeClass('disabled ent');
+			$('#cartopress_locator #cpdb-geocode-values span input').attr('readonly', false);
 			$('#cartopress_locator #cpdb-geocode-values label').css('color','#666666');
 	    } else {
-	    	$('#cartopress_locator #cpdb-geocode-values span input').attr('disabled', true);
+	    	$('#cartopress_locator #cpdb-geocode-values span input').addClass('disabled');
+	    	$('#cartopress_locator #cpdb-geocode-values span input').attr('readonly', true);
 	    	$('#cartopress_locator #cpdb-geocode-values label').css('color','#AAAAAA');
 	    }
 	});
